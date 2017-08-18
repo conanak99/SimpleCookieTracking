cf4953831a3539b703c7368329f5597bd3e60c79
System <none>
2017-08-18 06:02:25 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
+  if (request.cookies.id) response.redirect('/home');
   response.sendFile(__dirname + '/views/index.html');
 });
 
@@ -34,11 +35,34 @@ app.post("/register", function(request, response) {
   const password = request.body.password;
   db.user.insert({username, password}, (err, result) => {
     console.log(result);      
-    const _id = result.id;
+    const _id = result._id;
     response.cookie('id', _id, { maxAge: 30*24*3600*100 } );
-    response.sendFile(__dirname + '/views/logined.html');
+    response.redirect('/home');
   });
 });
+
+app.get('/home', function(request, response){
+  if (!request.cookies.id) response.redirect('/');
+  response.sendFile(__dirname + '/views/logined.html');
+});
+
+app.get('/logout', function(request, response) {
+  response.clearCookie('id'); 
+  response.redirect('/');
+});
+
+app.get('/user', function(request, response) {
+  const id = request.cookies.id;
+  console.log(id);
+  db.mycollection.findOne({ _id: mongojs.ObjectId(id) }, function(err, doc) {
+    response.json(doc);
+  });
+});
+
+app.get('/log', function(request, response) {
+  
+});
+
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
