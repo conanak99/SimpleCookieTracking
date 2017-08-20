a602fb625472b37d0f296136c2b061a3f865651d
System <none>
2017-08-20 01:14:25 +0000

    Checkpoint

--- public/track.js
+++ public/track.js
@@ -1,4 +1,13 @@
-fetch('https://sleepy-fuel.glitch.me/logWrite', {
+// Add iFrame to set cookie
+var iframe = document.createElement('iframe');
+iframe.style.display = "none";
+iframe.src = "https://sleepy-fuel.glitch.me";
+document.body.appendChild(iframe);
+
+setTimeout(() => {
+  fetch('https://sleepy-fuel.glitch.me/logWrite', {
         method: 'GET',
         credentials: 'include'
-});
+  });
+}, 1000);
+
